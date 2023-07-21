63688d96627b3330babb94d98d14b75bbf4fdd26
Pranalinannavare-5 <137520120+Pranalinannavare-5@users.noreply.github.com>
2023-07-21 13:19:33 +0530

    Add files via upload

---
+++ RE.js
@@ -0,0 +1,86 @@
+function a(){
+    document.getElementById("kk").src="./images/continental-gt-home-motorcycle2.avif"
+}
+function b(){
+    document.getElementById("kk").src="./images/super-meteor-6503.avif"
+}
+function c(){
+    document.getElementById("kk").src="./images/hunter-350-motorcycle-home 4.avif" 
+}
+function d(){
+    document.getElementById("kk").src="./images/royal-enfield-scram-411-home-motorcycle 5.avif"
+}
+function e(){
+    document.getElementById("kk").src="./images/classic-350-motorcycle-section6.avif"
+}
+function f(){
+    document.getElementById("kk").src="./images/royal-enfield-meteor-new-banner7.avif"
+}
+function g(){
+    document.getElementById("kk").src="./images/sleethblack-banner-des8.avif"
+}
+function h(){
+    document.getElementById("kk").src="./images/banner9.avif" 
+}
+function i(){
+    document.getElementById("kk").src="./images/interceptor-home-motorcycle1.avif"
+}
+
+// imagesection2
+function j(){
+    document.getElementById("ll").src="./images/rides2.avif"
+}
+function k(){
+    document.getElementById("ll").src="./images/rides3.avif"
+}
+function l(){
+    document.getElementById("ll").src="./images/rides4.avif"
+}
+function m(){
+    document.getElementById("ll").src="./images/rides5.avif"
+}
+function n(){
+    document.getElementById("ll").src="./images/rides6.avif"
+}
+function o(){
+    document.getElementById("ll").src="./images/himalayan-odyssey-2023 headersec2.avif"
+}
+
+// imagesection3
+function p(){
+    document.getElementById("mm").src="./images/apparel2.avif"
+}
+function q() {
+    document.getElementById("mm").src="./images/apparel3.avif" 
+
+}
+function r(){
+    document.getElementById("mm").src="./images/apparel4.avif"
+}
+function s(){
+    document.getElementById("mm").src="./images/apparel5.avif"
+}
+function t(){
+    document.getElementById("mm").src="./images/apparel6.avif"
+}
+function u(){
+    document.getElementById("mm").src="./images/art-of-motorcycling-apparel headersec3.avif"
+}
+//  imagesection4
+function v(){
+    document.getElementById("nn").src="./images/accessories 2.avif"
+}
+function w(){
+    document.getElementById("nn").src="./images/gma-home headersec4.avif"
+}
+
+// imagesection5
+function x(){
+    document.getElementById("oo").src="./images/locateus 2.avif"
+}
+function y(){
+    document.getElementById("oo").src="./images/locateus 3.avif" 
+}
+function z(){
+    document.getElementById("oo").src="./images/stores headersec5.avif"
+}
